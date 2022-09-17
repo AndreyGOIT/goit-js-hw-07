3257c8d05b0fa0877d3e86353409d4bddca36416
AndreyGOIT <105428939+AndreyGOIT@users.noreply.github.com>
2022-09-17 13:57:25 +0300

    update 02-lighthbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+const markup = galleryItems
+  .map(
+    (galleryItem) => `<div class="gallery__item">
+  <a class="gallery__item" href="${galleryItem.original}" >
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" title="${galleryItem.description}" captionDelay="250ms"/>
+</a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+console.log(gallery);
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', { /* option */ });
