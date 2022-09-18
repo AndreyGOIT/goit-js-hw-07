84447c575f86425fd45a160c58902d86a5bd9b36
AndreyGOIT <ae.borga@gmail.com>
2022-09-18 09:50:20 +0300

    HW7 task1 is ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,61 +1,58 @@
 // Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
 
-// 1. Создание и рендер разметки по массиву данных galleryItems и 
+// 1. Создание и рендер разметки по массиву данных galleryItems и
 // предоставленному шаблону элемента галереи.
 // 2. Реализация делегирования на div.gallery и получение url большого изображения.
-// 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox. 
+// 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox.
 // Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
-// 4. Открытие модального окна по клику на элементе галереи. 
+// 4. Открытие модального окна по клику на элементе галереи.
 // Для этого ознакомься с документацией и примерами.
-// 5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. 
-// 6. Используй готовую разметку модального окна с изображением из 
+// 5. Замена значения атрибута src элемента <img> в модальном окне перед открытием.
+// 6. Используй готовую разметку модального окна с изображением из
 // примеров библиотеки basicLightbox.
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", onGalleryClick);
 
 const markup = galleryItems
-    .map((galleryItem) => `<div class="gallery__item">
-  <a class="gallery__link" href="${galleryItem.original}" target="_self">
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}" target="_self">
     <img
       class="gallery__image"
-      src="${galleryItem.preview}"
-      data-source="${galleryItem.original}"
-      alt="${galleryItem.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</div>`)
-    .join("");
+</div>`
+  )
+  .join("");
 
 gallery.insertAdjacentHTML("beforeend", markup);
 console.log(gallery);
 
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
 
-// refs.gallery.addEventListener('click', handleClick);
+  const instance = basicLightbox.create(
+    `
+    <img src='${event.target.dataset.source}' width="800" height="600">
+`,
+    {
+      onShow: () => document.addEventListener("keydown", onEscBtnPress),
+      onClose: () => document.removeEventListener("keydown", onEscBtnPress),
+    }
+  );
 
-// function handleClick(evt) {
-//     console.log(evt.target);
-//     console.log(evt.currentTarget);
-    
-//   console.log("Button was clicked");
-// };
+  instance.show();
 
-
-/* …or skip the JS file and use basicLightbox as a module:
- */
-// import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
-
-// instance.show();
-
-
-document.addEventListener("keydown", event => {
-    event.preventDefault();
-
-  if (event.key && event.code === "Escape") {
-    console.log("Закрываем модальное окно");
+  function onEscBtnPress(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
   }
-});
+}
